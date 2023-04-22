ed8b294e30c965d58db870eb9d8f49090d74d5ac
bsk1191 <102401915+bsk1191@users.noreply.github.com>
2023-04-22 19:44:02 +0530

    Update ReverseANumber.java

--- Logical_programming/ReverseANumber.java
+++ Logical_programming/ReverseANumber.java
@@ -5,7 +5,7 @@ class ReverseANumber {
 		System.out.print("Enter A Number: ");
 		int num=scn.nextInt();
 		int rev=0;
-		while(num!=0){ 			//iterating until the number becomes false(i.e 0).
+		while(num!=0){ 			//iterating until the condition becomes false(i.e until number becomes 0).
 			rev=rev*10+num%10;	//Extracting the last digit and adding to rev variable
 			num=num/10;		//Elemenating the last digit from the number.
 		}
