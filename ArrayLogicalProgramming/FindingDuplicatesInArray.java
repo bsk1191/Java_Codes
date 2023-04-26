10a70535f071329260746ce4232b42f6db721333
bsk1191 <102401915+bsk1191@users.noreply.github.com>
2023-04-26 20:47:41 +0530

    Create FindingDuplicatesInArray.java

---
+++ ArrayLogicalProgramming/FindingDuplicatesInArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class FindingDuplicatesInArray{
+	public static void main(String[] args){
+		Scanner scn=new Scanner(System.in);
+		System.out.print("Enter the size of the array: ");
+		int size=scn.nextInt();
+		int array[]=new int[size];
+
+		System.out.println("Enter the elements of the array: ");
+		for (int i=0;i<array.length ;i++ )
+			array[i]=scn.nextInt();
+
+		boolean found=false;														         		// Assume that there is no duplicate element
+
+		for (int i=0;i<array.length ;i++ )													// Take first element from the array.
+			for (int j=i+1;j<array.length ;j++ )											// Compare the first element with remaining values
+				if (array[i]==array[j]){
+					System.out.println("The Duplicate Element is found: "+array[j]);
+					found=true;
+		}
+		if (found==false)
+			System.out.println("Duplicate Element is NOT found");
+		
+	}
+}
