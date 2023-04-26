257ec0dcdcc016c1cddb1db856fd17ea682f563f
bsk1191 <102401915+bsk1191@users.noreply.github.com>
2023-04-26 20:08:55 +0530

    Update MinAndMaxElementInArray.java

--- ArrayLogicalProgramming/MinAndMaxElementInArray.java
+++ ArrayLogicalProgramming/MinAndMaxElementInArray.java
@@ -14,11 +14,17 @@ class MinAndMaxElementInArray{
 
 		int max=array[0];							// Assume the first element as max element.
 
-		for (int i=1;i<array.length;i++ )			// Retrieve the remaining elements.
+		for (int i=1;i<array.length;i++ )			                // Retrieve the remaining elements.
 			if (array[i]>max)						// Compare the remaing elements with max variable if true.
 				max=array[i];						// Assign that element to the max variable.
 			
+		int min=array[0];
+		for (int i=1;i<array.length;i++ )			
+			if (array[i]<min)						
+				min=array[i];
+			
 		
 		System.out.println("The max element value is: "+max);
+		System.out.println("The min element value is: "+min);
 	}
 }
