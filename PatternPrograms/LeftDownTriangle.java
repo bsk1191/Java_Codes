8170f2681edc9ecdab2d031f98ae268175791b42
bsk1191 <102401915+bsk1191@users.noreply.github.com>
2023-04-29 21:19:30 +0530

    Update LeftDownTriangle.java

--- PatternPrograms/LeftDownTriangle.java
+++ PatternPrograms/LeftDownTriangle.java
@@ -1,6 +1,6 @@
 class  LeftDownTriangle{
 	public static void main(String[] args) {
-		for (int i=1;i<=5 ;i++ ){					//Represents rows
+		for (int i=1;i<=5 ;i++ ){					
 			for (int j=1;j<i ;j++ ){
 				System.out.print("  ");
 			}
